fd9bb0f22c8dbe26720c50881abd71737e73b9cf
Valentin Destruel <destruel.valentin@gmail.com>
2018-04-05 15:28:44 +0200

    Changements intents

--- app/src/main/java/fr/wcs/warofheroes/MainActivity.java
+++ app/src/main/java/fr/wcs/warofheroes/MainActivity.java
@@ -4,14 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONObject;
 import android.view.View;
 import android.widget.Button;
@@ -60,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent intentVersus = new Intent(MainActivity.this, ArenaActivity.class);
+                Intent intentVersus = new Intent(MainActivity.this, ChooseHeroesActivity.class);
                 startActivity(intentVersus);
             }
         });
@@ -69,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 
-                Intent intentTraining = new Intent(MainActivity.this, ArenaActivity.class);
+                Intent intentTraining = new Intent(MainActivity.this, ChooseHeroesActivity.class);
                 startActivity(intentTraining);
             }
         });
