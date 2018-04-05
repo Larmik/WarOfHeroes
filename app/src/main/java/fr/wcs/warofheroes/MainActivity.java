8e60657808bae6aec4f7c733821dc9ba9562029f
Alex1384 <a.peyrat84@gmail.com>
2018-04-05 15:24:56 +0200

    glide_picture

--- app/src/main/java/fr/wcs/warofheroes/MainActivity.java
+++ app/src/main/java/fr/wcs/warofheroes/MainActivity.java
@@ -2,6 +2,9 @@ package fr.wcs.warofheroes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView ivLogo = findViewById(R.id.image_glide);
+        String url = "https://wildcodeschool.fr/wp-content/uploads/2017/01/logo_orange_pastille.png";
+        Glide.with(this).load(url) .into(ivLogo);
+
+
+
+
     }
 }
