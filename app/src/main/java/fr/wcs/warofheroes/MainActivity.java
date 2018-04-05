d5ebd690f0d4450acc8ae6c7b6583a78d2bd8244
Valentin Destruel <destruel.valentin@gmail.com>
2018-04-05 14:58:34 +0200

    Page d'accueil

--- app/src/main/java/fr/wcs/warofheroes/MainActivity.java
+++ app/src/main/java/fr/wcs/warofheroes/MainActivity.java
@@ -1,7 +1,10 @@
 package fr.wcs.warofheroes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonVersus = findViewById(R.id.button_versus);
+        Button buttonTraining = findViewById(R.id.button_training);
+
+        buttonVersus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intentVersus = new Intent(MainActivity.this, ArenaActivity.class);
+                startActivity(intentVersus);
+            }
+        });
+        
+        buttonTraining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+                Intent intentTraining = new Intent(MainActivity.this, ArenaActivity.class);
+                startActivity(intentTraining);
+            }
+        });
     }
 }
