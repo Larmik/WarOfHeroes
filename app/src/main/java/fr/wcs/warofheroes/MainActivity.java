ef1c0d562942c89a109af0de2bed253badc49b9a
Pascal Alberti <pasalberti@gmail.com>
2018-04-05 15:36:24 +0200

    Merge de Glide

--- app/src/main/java/fr/wcs/warofheroes/MainActivity.java
+++ app/src/main/java/fr/wcs/warofheroes/MainActivity.java
@@ -4,14 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONObject;
 import android.view.View;
 import android.widget.Button;
@@ -23,35 +21,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        // Crée une file d'attente pour les requêtes vers l'API
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        String url = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json";
-
-        // Création de la requête vers l'API, ajout des écouteurs pour les réponses et erreurs possibles
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        // TODO : traiter la réponse
-                    }
-                },
-                new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        // Afficher l'erreur
-                        Log.d("VOLLEY_ERROR", "onErrorResponse: " + error.getMessage());
-                    }
-                }
-        );
-
-        // On ajoute la requête à la file d'attente
-        requestQueue.add(jsonObjectRequest);
 
         Button buttonVersus = findViewById(R.id.button_versus);
         Button buttonTraining = findViewById(R.id.button_training);
