cf19ef8097b7b3523643265f55f6869eb09d716a
Pascal Alberti <pasalberti@gmail.com>
2018-04-06 11:15:24 +0200

    activity_resume_buttons

--- app/src/main/java/fr/wcs/warofheroes/ResumeFightActivity.java
+++ app/src/main/java/fr/wcs/warofheroes/ResumeFightActivity.java
@@ -1,7 +1,11 @@
 package fr.wcs.warofheroes;
 
+import android.content.Intent;
+import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResumeFightActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class ResumeFightActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume_fight);
+
+        Button bLeave = findViewById(R.id.button_leave_game);
+        bLeave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToMainActivity = new Intent(ResumeFightActivity.this, MainActivity.class);
+                startActivity(goToMainActivity);
+            }
+        });
+
+        Button bStartAgain = findViewById(R.id.button_start);
+        bStartAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gotoChooseHeroes = new Intent(ResumeFightActivity.this,ChooseHeroesActivity.class);
+                startActivity(gotoChooseHeroes);
+            }
+        });
+
     }
 }
